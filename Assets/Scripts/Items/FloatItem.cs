c9f3f6b07a6ccb7e79b7462cbe9b119e40c137bf
Forgrul <hngieng9@gmail.com>
2024-11-07 10:59:12 +0800

    new tilemap

--- Assets/Scripts/Items/FloatItem.cs
+++ Assets/Scripts/Items/FloatItem.cs
@@ -3,13 +3,17 @@ using System.Collections;
 
 public class FloatItem : MonoBehaviour
 {
+    public float respawnTime = 10f;
+    public float duration = 5f;
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         // 檢查碰撞的物件是否是 TargetObject
-        if (collision.gameObject.CompareTag("Player")) // 確保已設置標籤
+        if (collision.gameObject.CompareTag("Player") && GetComponent<SpriteRenderer>().enabled) // 確保已設置標籤
         {
             GameManager.Instance.ChangeFloatStatus(true);
             StartCoroutine(BackToGround());
+            StartCoroutine(DisappearAndRespawn());
             // collision.gameObject.GetComponent<PlayerController>().CollectFloatItem();
         }
     }
@@ -17,9 +21,16 @@ public class FloatItem : MonoBehaviour
     IEnumerator BackToGround()
     {
         // Wait for 5 seconds
-        yield return new WaitForSeconds(5f);
+        yield return new WaitForSeconds(duration);
 
         // Print the message to the console
         GameManager.Instance.ChangeFloatStatus(false);
+    }
+
+    IEnumerator DisappearAndRespawn()
+    {
+        GetComponent<SpriteRenderer>().enabled = false;
+        yield return new WaitForSeconds(respawnTime);
+        GetComponent<SpriteRenderer>().enabled = true;
     }
 }
